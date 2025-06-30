ef7feeb9906c6a7a544d192bdde4517bcd4cf775
hoangdan123pg <dannhhe173549@fpt.edu.vn>
2025-06-30 21:35:37 +0700

    test update base code

--- app/src/main/java/com/example/project_quiz_app/MainActivity.java
+++ app/src/main/java/com/example/project_quiz_app/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
