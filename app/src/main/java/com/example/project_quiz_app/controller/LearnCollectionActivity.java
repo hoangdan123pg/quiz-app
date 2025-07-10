dc3cbd196e4bfe399571a8ebbf5c5aa0105938a1
hoangdan123pg <dannhhe173549@fpt.edu.vn>
2025-07-10 20:27:30 +0700

    update ui home activity
    moi xong ui, chua connect to room db

---
+++ app/src/main/java/com/example/project_quiz_app/controller/LearnCollectionActivity.java
@@ -0,0 +1,37 @@
+package com.example.project_quiz_app.controller;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.project_quiz_app.R;
+
+public class LearnCollectionActivity extends AppCompatActivity {
+    private Button btnBack;
+
+    private void bindingView() {
+        btnBack = findViewById(R.id.btnBack);
+    }
+    private void bindingAction() {
+        btnBack.setOnClickListener(view -> finish());
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_learn_collection);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        bindingView();
+        bindingAction();
+    }
+}
